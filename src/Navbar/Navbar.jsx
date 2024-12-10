eb73b8c6b41990784fc811a93f375174c26b5bf5
Sanket Ghoagre <sanket_vasant.ghogare@syngenta.com>
2024-12-10 18:57:29 +0530

    add the test colur

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -59,7 +59,7 @@ const Navbar = () => {
             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
               <div className="flex flex-shrink-0 items-center">
                 <div className="h-8 w-auto  ">
-                                <h1 className="font-bold text-2xl ss:text-base"> Maharashtra-Sarkari Yogana</h1>
+                                <h1 className="font-bold text-2xl ss:text-base text-black hover:bg-gray-300 dark:text-white  hover:text-black "> Maharashtra-Sarkari Yogana</h1>
 
                 </div> 
               </div>
